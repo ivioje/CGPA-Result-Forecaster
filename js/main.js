e4f8f24a3685013f8d43765ea56a498e97232d51
ivioje <rhodaivioje@gmail.com>
2021-02-12 21:51:49 +0100

    added function to forecast results

--- js/main.js
+++ js/main.js
@@ -114,3 +114,57 @@ function continueCalculation () {
     console.log(clickedYesBtn);
   }
 }
+
+// 4.50-5.00         1st Class
+// 3.5-4.49          2nd Class Upper
+// 2.50-3.49         2nd Class Lower
+// 1.50-2.49         3rd Class
+// 1.00-1.49         Pass
+// 0.-0.99           Fail
+
+// Forecast Result when no button is clicked
+function stopCalculation () {
+  let firstClass = averageGPA(years.value, 4.52);
+  let content = `
+    <div class="display__result__content">
+      <h3>${firstUpper(userName.value)} you're on a ${
+    years.value
+  } years program</h3>
+      <p><strong>${cgpaCal(cgpaArray)}</strong> is your current CGPA score</p>
+
+      <small><em>Note: The average score is approximated</em></small>
+
+      <div>
+        ${
+          firstClass > 4.9
+            ? ''
+            : `<p>You'll need <strong>${firstClass}</strong> to remain at <strong>1st class</strong> for each of the semesters left</p>`
+        }
+
+        <p>You'll need <strong>${averageGPA(
+          years.value,
+          3.52
+        )}</strong> to remain at with a <strong>2nd class upper</strong> for each of the semesters left</p>
+
+        <p>You'll need <strong>${averageGPA(
+          years.value,
+          2.52
+        )}</strong> to remain at with a <strong>2nd class lower</strong> for each of the semesters left</p>
+        
+      </div>
+
+      <button class="closeBtn">Close</button>
+    </div>
+    
+  `;
+  displayBg.style.display = 'flex';
+  displayResult.style.display = 'flex';
+  displayResult.innerHTML = content;
+
+  let closeBtn = call('.closeBtn');
+
+  closeBtn.addEventListener('click', () => {
+    displayBg.style.display = 'none';
+    displayResult.style.display = 'none';
+  });
+}
